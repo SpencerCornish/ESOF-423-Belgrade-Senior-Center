e1f60e2e737be3c6647bd8b8116b84e35b03a43e
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2019-02-10 11:41:44 -0700

    add: types/single get methods. update refNames to match dbRefs. code cleanup for clarity

--- lib/src/firebase/firebaseClient.dart
+++ lib/src/firebase/firebaseClient.dart
@@ -34,22 +34,39 @@ class FirebaseClient {
     print("Auth Changed :: $user");
   }
 
-  // getMembers (with role or all?)
-  getMembers(){
-    return _refs.userRef().get();
+  // getMembers (TODO: with role or all?)
+  //this will get all
+  getAllMembers(){
+    return _refs.allUsers().get();
   }
-  // getMeals (by date/date range?)
-  getMeals(){
-    return _refs.userRef().get();
+  //...and just one
+  getMember(String uid){
+    return _refs.user(uid).get();
   }
-  // getClasses (by day, range?)
-  getClasses(){
-    return _refs.userRef().get();
+
+  // getMeals (TODO: by date/date range?)
+  //this will get all
+  getAllMeals(){
+    return _refs.allMeals().get();
+  }
+  //...and just one
+  getMeal(String uid){
+    return _refs.meal(uid).get();
+  }
+
+  // getClasses (TODO: by day, range?)
+  //this will get all
+  getAllClasses(){
+    return _refs.allClasses().get();
+  }
+  //...and just one
+  getclass(String uid){
+    return _refs.singleClass(uid).get();
   }
 
   //create new user/class/meal
-  createUser(name, email, phone, password, role, classes){
-      _refs.userRef().add({
+  createUser(String name, String email, String phone, String password, String role, List classes){
+      _refs.allUsers().add({
         "name": name,
         "email": email,
         "phone_number": phone,
@@ -57,9 +74,8 @@ class FirebaseClient {
         "role": role
       });
   }
-
-  createClass(name, start, end, instructor, location, capacity){
-      _refs.classRef().add({
+  createClass(String name, String start, String end, String instructor, String location, int capacity){
+      _refs.allClasses().add({
         "name": name,
         "start_time": start,
         "end_time": end,
@@ -68,48 +84,36 @@ class FirebaseClient {
         "capacity": capacity
       });
   }
-
-  createMeal(start, end, menu){
-      _refs.mealRef().add({
+  createMeal(String start, String end, List menu){
+      _refs.allMeals().add({
         "start_time": start,
         "end_time": end,
         "menu": menu
       });
   }  
-  //update existing user/class/meal (especially date)
-  updateUser(String documentID, String key, value){
-    Map update = new Map<String, Object>();
-    update.putIfAbsent(key, value);
-    
-    _refs.userRef().document(documentID)
-      .set(update, merge : true);
+
+  //update existing user/class/meal
+  updateUser(String documentID, String key, Object value){
+    _refs.user(documentID)
+      .set(key, value, merge : true);
+  }
+  updateClass(String documentID, String key, Object value){    
+    _refs.singleClass(documentID)
+      .set(key, value, merge : true);
+  }
+  updateMeal(String documentID, String key, Object value){
+    _refs.meal(documentID)
+      .set(key: value, merge : true);
   }
 
-  updateClass(String documentID, String key, value){
-    Map update = new Map<String, Object>();
-    update.putIfAbsent(key, value);
-    
-    _refs.classRef().document(documentID)
-      .set(update, merge : true);
-  }
-
-  updateMeal(String documentID, String key, value){
-    Map update = new Map<String, Object>();
-    update.putIfAbsent(key, value);
-    
-    _refs.mealRef().document(documentID)
-      .set(update, merge : true);
-  }
   //delete existing
   deleteUser(String documentID){
-    _refs.userRef().document(documentID).delete();
+    _refs.user(documentID).delete();
   }
-
   deleteClass(String documentID){
-    _refs.classRef().document(documentID).delete();
+    _refs.singleClass(documentID).delete();
   }
-
   deleteMeal(String documentID){
-    _refs.mealRef().document(documentID).delete();
+    _refs.meal(documentID).delete();
   }
 }
