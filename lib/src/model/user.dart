88b0579f2f211fb90ed108afa34e0332981bc0ef
Dan <daniel.bachler@comcast.net>
2019-02-18 14:39:15 -0700

    Add new files for model files

--- lib/src/model/user.dart
+++ lib/src/model/user.dart
@@ -13,41 +13,41 @@ abstract class User implements Built<User, UserBuilder> {
   /// [uid] is the unique identifier for the user
   String get uid;
 
-  // TODO: Hudrate these fields when creating a user model
-  // /// [firstName] required
-  // String get firstName;
+  TODO: Hudrate these fields when creating a user model
+  /// [firstName] required
+  String get firstName;
 
-  // /// [lastName] required
-  // String get lastName;
+  /// [lastName] required
+  String get lastName;
 
-  // /// [email] may be an empty string
-  // String get email;
+  /// [email] may be an empty string
+  String get email;
 
-  // /// [number] may be an empty string
-  // String get number;
+  /// [number] may be an empty string
+  String get number;
 
-  // /// [cellNumber] may be an empty string
-  // String get cellNumber;
+  /// [cellNumber] may be an empty string
+  String get cellNumber;
 
-  // /// [address] US only
-  // String get address;
+  /// [address] US only
+  String get address;
 
-  // /// [role] is an enumeration of the users' role. This controls access levels, as well as the pages shown to the user.
-  // /// All of this is backed up with proper database rules, so maliciously subverting the user role locally will have
-  // /// no effect.
-  // Role get role;
+  /// [role] is an enumeration of the users' role. This controls access levels, as well as the pages shown to the user.
+  /// All of this is backed up with proper database rules, so maliciously subverting the user role locally will have
+  /// no effect.
+  Role get role;
 
-  // /// [dietaryRestrictions] is a comma separated list of restricted dietary items for the user
-  // String get dietaryRestrictions;
+  /// [dietaryRestrictions] is a comma separated list of restricted dietary items for the user
+  String get dietaryRestrictions;
 
-  // /// [emergencyContacts] is a built list of [EmergencyContact] objects
-  // BuiltList<String> get emergencyContacts;
+  /// [emergencyContacts] is a built list of [EmergencyContact] objects
+  BuiltList<String> get emergencyContacts;
 
-  // /// [membershipStart] is a DateTime object representing the time at which the user's membership began
-  // DateTime get membershipStart;
+  /// [membershipStart] is a DateTime object representing the time at which the user's membership began
+  DateTime get membershipStart;
 
-  // /// [membershipRenewal] is a DateTime object representing the time at which the user's membership began
-  // DateTime get membershipRenewal;
+  /// [membershipRenewal] is a DateTime object representing the time at which the user's membership began
+  DateTime get membershipRenewal;
 
   User._();
   factory User([updates(UserBuilder b)]) = _$User;
