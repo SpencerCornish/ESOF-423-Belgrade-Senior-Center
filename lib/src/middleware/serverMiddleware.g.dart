0a933d3c7db8f1fe2be10add888a2472067d8eff
spencercornish <SpencerCornish@users.noreply.github.com>
2019-02-24 14:00:30 -0700

    fmt

--- lib/src/middleware/serverMiddleware.g.dart
+++ lib/src/middleware/serverMiddleware.g.dart
@@ -14,12 +14,9 @@ class _$ServerMiddlewareActions extends ServerMiddlewareActions {
   _$ServerMiddlewareActions._() : super._();
 
   final ActionDispatcher<AdminSignInPayload> signInAdmin =
-      new ActionDispatcher<AdminSignInPayload>(
-          'ServerMiddlewareActions-signInAdmin');
-  final ActionDispatcher<Null> logOut =
-      new ActionDispatcher<Null>('ServerMiddlewareActions-logOut');
-  final ActionDispatcher<String> resetPassword =
-      new ActionDispatcher<String>('ServerMiddlewareActions-resetPassword');
+      new ActionDispatcher<AdminSignInPayload>('ServerMiddlewareActions-signInAdmin');
+  final ActionDispatcher<Null> logOut = new ActionDispatcher<Null>('ServerMiddlewareActions-logOut');
+  final ActionDispatcher<String> resetPassword = new ActionDispatcher<String>('ServerMiddlewareActions-resetPassword');
 
   @override
   void setDispatcher(Dispatcher dispatcher) {
@@ -32,8 +29,6 @@ class _$ServerMiddlewareActions extends ServerMiddlewareActions {
 class ServerMiddlewareActionsNames {
   static final ActionName<AdminSignInPayload> signInAdmin =
       new ActionName<AdminSignInPayload>('ServerMiddlewareActions-signInAdmin');
-  static final ActionName<Null> logOut =
-      new ActionName<Null>('ServerMiddlewareActions-logOut');
-  static final ActionName<String> resetPassword =
-      new ActionName<String>('ServerMiddlewareActions-resetPassword');
+  static final ActionName<Null> logOut = new ActionName<Null>('ServerMiddlewareActions-logOut');
+  static final ActionName<String> resetPassword = new ActionName<String>('ServerMiddlewareActions-resetPassword');
 }
