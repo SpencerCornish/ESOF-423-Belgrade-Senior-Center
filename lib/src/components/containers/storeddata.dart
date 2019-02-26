c24f570dc437423c285842ec0b1c094a314033b4
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2019-02-25 17:04:53 -0700

    reformat title

--- lib/src/components/containers/storeddata.dart
+++ lib/src/components/containers/storeddata.dart
@@ -64,7 +64,7 @@ class StoredData extends PComponent<StoredDataProps> {
       i++;
       nodeList.add(
         new VTableCellElement()
-          ..className = 'title is-4'
+          ..className = 'title is-5'
           ..text = "Title " + i.toString(),
       );
     }
@@ -87,48 +87,40 @@ class StoredData extends PComponent<StoredDataProps> {
                 ..className = 'column is-four-fifths'
                 ..children = [
                   new VDivElement()
-                    ..className = 'columns is-mobile is-centered'
-                    ..children = [
-                      new VDivElement()
-                        ..className = 'column is-narrow'
-                        ..children = [
-                          new Vh1()
-                            ..className = 'title'
-                            ..text = 'Stored Data',
-                          new Vh1()
-                            ..className = 'subtitle'
-                            ..text = ' as of: ' + DateTime.now().toString().split(" ")[0],
-                        ],
-                    ],
-                  new VDivElement()
                     ..className = 'box'
                     ..children = [
                       new VDivElement()
-                        ..className = 'buttons are-medium is-centered'
+                        ..className = 'columns is-mobile'
                         ..children = [
-                          new Va()
-                            ..className = 'button'
-                            ..text = 'Users',
-                          new Va()
-                            ..className = 'button'
-                            ..text = 'Activity',
-                          new Va()
-                            ..className = 'button'
-                            ..text = 'Meals',
-                        ],
-                      new VDivElement()
-                        ..className = 'field'
-                        ..children = [
-                          new VParagraphElement()
-                            ..className = 'control has-icons-left'
+                          new VDivElement()
+                            ..className = 'column is-narrow'
                             ..children = [
-                              new VInputElement()
-                                ..className = 'input'
-                                ..placeholder = 'Filter'
-                                ..type = 'text',
-                              new VSpanElement()
-                                ..className = 'icon is-left'
-                                ..children = [new Vi()..className = 'fas fa-search'],
+                              new Vh4()
+                                ..className = 'title is-4'
+                                ..text = 'Stored Data',
+                              new Vh1()
+                                ..className = 'subtitle is-7'
+                                ..text = ' as of: ' + DateTime.now().toString().split(" ")[0],
+                            ],
+                          new VDivElement()..className = 'column',
+                          new VDivElement()
+                            ..className = 'column is-4'
+                            ..children = [
+                              new VDivElement()
+                                ..className = 'field'
+                                ..children = [
+                                  new VParagraphElement()
+                                    ..className = 'control has-icons-left'
+                                    ..children = [
+                                      new VInputElement()
+                                        ..className = 'input'
+                                        ..placeholder = 'Filter'
+                                        ..type = 'text',
+                                      new VSpanElement()
+                                        ..className = 'icon is-left'
+                                        ..children = [new Vi()..className = 'fas fa-search'],
+                                    ],
+                                ],
                             ],
                         ],
                       new VTableElement()
