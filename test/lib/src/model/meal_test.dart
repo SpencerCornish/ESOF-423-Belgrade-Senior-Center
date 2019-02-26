9be498cc0134d5bf1c9979ad6df49ef1d00120b8
Daniel Bachler <daniel.bachler@comcast.net>
2019-02-25 18:12:09 -0700

    More framework for unit testing

--- test/lib/src/model/meal_test.dart
+++ test/lib/src/model/meal_test.dart
@@ -2,5 +2,11 @@ import 'package:test/test.dart';
 import 'package:bsc/src/model/meal.dart';
 
 void main() {
-  
+  test('Make sure data from firebase is accuratly changed into data for dart', () {
+    
+  });
+
+  test('Make sure that data from dart is accuratly changed into data for firebase', () {
+    
+  });
 }
