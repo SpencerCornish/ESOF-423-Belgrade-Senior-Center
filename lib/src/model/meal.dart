40e4310a32185ba247f4cdb8210f7b41839a31d4
Daniel Bachler <daniel.bachler@comcast.net>
2019-02-25 18:08:28 -0700

    Fix missing files

---
+++ lib/src/model/meal.dart
@@ -0,0 +1,38 @@
+library meal;
+
+import 'package:built_value/built_value.dart';
+import 'package:built_collection/built_collection.dart';
+
+part 'meal.g.dart';
+
+/// [Meal] is a model for the user database document
+abstract class Meal implements Built<Meal, MealBuilder> {
+  /// [uid] is the unique identifier for the meal
+  String get uid;
+
+  /// [startTime] required
+  DateTime get startTime;
+
+  /// [endTime] required
+  DateTime get endTime;
+
+  /// [menu] required
+  /// A list of strings that represent the menu for a given meal time.
+  BuiltList<String> get menu;
+
+  Meal._();
+  factory Meal([updates(MealBuilder b)]) = _$Meal;
+
+  factory Meal.fromFirebase(Map<String, dynamic> firestoreData) => new Meal((MealBuilder builder) => builder
+    ..uid = firestoreData['uid']
+    ..startTime = DateTime.parse(firestoreData['start_time'])
+    ..endTime = DateTime.parse(firestoreData['end_time'])
+    ..menu = new BuiltList<String>.from(firestoreData['menu']).toBuilder());
+
+  Map<String, dynamic> toFirestore() => {
+        'uid': uid,
+        'start_time': startTime.toIso8601String(),
+        'end_time': endTime.toIso8601String(),
+        'menu': menu.toList(),
+      };
+}
