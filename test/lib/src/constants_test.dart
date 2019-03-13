027cd184d074c8a4e4b555eed79d5ce0a6f26f25
Dan Bachler <daniel.bachler@comcast.net>
2019-03-13 10:44:07 -0600

    Add unit testing framework for constants.dart

---
+++ test/lib/src/constants_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import 'package:bsc/src/constants.dart';
+
+void main() {
+  //Vars for testing
+
+  group('Constants -', () {
+    test('emailIsValid correctly checks emails', () {
+      
+    });
+
+    test('passwordIsValid correctly checks passwords', () {
+      
+    });
+
+    test('stringToBase properly encodes strings', () {
+      
+    });
+
+    test('baseToString properly decodes strings', () {
+      
+    });
+  });
+}
