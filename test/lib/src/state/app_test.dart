d277e3257fea2e225c947c125ee0d08ab915f3ec
Dan Bachler <daniel.bachler@comcast.net>
2019-03-14 13:10:24 -0600

    Add framework for testing all 3 classes listed in issue

---
+++ test/lib/src/state/app_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import 'package:bsc/src/state/app.dart';
+
+void main() {
+  //Vars for testing
+
+  group('App -', () {
+    test('emailIsValid correctly checks emails', () {
+      
+    });
+
+    test('passwordIsValid correctly checks passwords', () {
+      
+    });
+
+    test('stringToBase properly encodes strings', () {
+      
+    });
+
+    test('baseToString properly decodes strings', () {
+      
+    });
+  });
+}
