7abc7e535140178b122d9dbd262e3e2327733b58
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2019-03-29 11:13:04 -0600

    format

--- lib/src/model/user.g.dart
+++ lib/src/model/user.g.dart
@@ -46,8 +46,7 @@ class _$User extends User {
   @override
   final BuiltList<String> services;
 
-  factory _$User([void updates(UserBuilder b)]) =>
-      (new UserBuilder()..update(updates)).build();
+  factory _$User([void updates(UserBuilder b)]) => (new UserBuilder()..update(updates)).build();
 
   _$User._(
       {this.loginUID,
@@ -118,8 +117,7 @@ class _$User extends User {
   }
 
   @override
-  User rebuild(void updates(UserBuilder b)) =>
-      (toBuilder()..update(updates)).build();
+  User rebuild(void updates(UserBuilder b)) => (toBuilder()..update(updates)).build();
 
   @override
   UserBuilder toBuilder() => new UserBuilder()..replace(this);
@@ -167,21 +165,11 @@ class _$User extends User {
                                                         $jc(
                                                             $jc(
                                                                 $jc(
-                                                                    $jc(
-                                                                        $jc(
-                                                                            $jc(
-                                                                                0,
-                                                                                loginUID
-                                                                                    .hashCode),
-                                                                            docUID
-                                                                                .hashCode),
-                                                                        firstName
-                                                                            .hashCode),
-                                                                    lastName
-                                                                        .hashCode),
+                                                                    $jc($jc($jc(0, loginUID.hashCode), docUID.hashCode),
+                                                                        firstName.hashCode),
+                                                                    lastName.hashCode),
                                                                 email.hashCode),
-                                                            phoneNumber
-                                                                .hashCode),
+                                                            phoneNumber.hashCode),
                                                         mobileNumber.hashCode),
                                                     address.hashCode),
                                                 role.hashCode),
@@ -264,8 +252,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   String _dietaryRestrictions;
   String get dietaryRestrictions => _$this._dietaryRestrictions;
-  set dietaryRestrictions(String dietaryRestrictions) =>
-      _$this._dietaryRestrictions = dietaryRestrictions;
+  set dietaryRestrictions(String dietaryRestrictions) => _$this._dietaryRestrictions = dietaryRestrictions;
 
   ListBuilder<EmergencyContact> _emergencyContacts;
   ListBuilder<EmergencyContact> get emergencyContacts =>
@@ -275,13 +262,11 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   DateTime _membershipStart;
   DateTime get membershipStart => _$this._membershipStart;
-  set membershipStart(DateTime membershipStart) =>
-      _$this._membershipStart = membershipStart;
+  set membershipStart(DateTime membershipStart) => _$this._membershipStart = membershipStart;
 
   DateTime _membershipRenewal;
   DateTime get membershipRenewal => _$this._membershipRenewal;
-  set membershipRenewal(DateTime membershipRenewal) =>
-      _$this._membershipRenewal = membershipRenewal;
+  set membershipRenewal(DateTime membershipRenewal) => _$this._membershipRenewal = membershipRenewal;
 
   String _disabilities;
   String get disabilities => _$this._disabilities;
@@ -293,8 +278,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   String _medicalIssues;
   String get medicalIssues => _$this._medicalIssues;
-  set medicalIssues(String medicalIssues) =>
-      _$this._medicalIssues = medicalIssues;
+  set medicalIssues(String medicalIssues) => _$this._medicalIssues = medicalIssues;
 
   String _position;
   String get position => _$this._position;
@@ -305,8 +289,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
   set homeDeliver(bool homeDeliver) => _$this._homeDeliver = homeDeliver;
 
   ListBuilder<String> _services;
-  ListBuilder<String> get services =>
-      _$this._services ??= new ListBuilder<String>();
+  ListBuilder<String> get services => _$this._services ??= new ListBuilder<String>();
   set services(ListBuilder<String> services) => _$this._services = services;
 
   UserBuilder();
@@ -387,8 +370,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
         _$failedField = 'services';
         services.build();
       } catch (e) {
-        throw new BuiltValueNestedFieldError(
-            'User', _$failedField, e.toString());
+        throw new BuiltValueNestedFieldError('User', _$failedField, e.toString());
       }
       rethrow;
     }
