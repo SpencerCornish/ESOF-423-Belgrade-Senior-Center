97e5d943bd80e53f8065ba64e55a2a77195cece1
Taylor <taylorkoth@gmail.com>
2019-04-25 17:03:45 -0600

    formatting

--- lib/src/model/user.g.dart
+++ lib/src/model/user.g.dart
@@ -56,8 +56,7 @@ class _$User extends User {
   @override
   final BuiltList<String> services;
 
-  factory _$User([void updates(UserBuilder b)]) =>
-      (new UserBuilder()..update(updates)).build();
+  factory _$User([void updates(UserBuilder b)]) => (new UserBuilder()..update(updates)).build();
 
   _$User._(
       {this.loginUID,
@@ -139,8 +138,7 @@ class _$User extends User {
   }
 
   @override
-  User rebuild(void updates(UserBuilder b)) =>
-      (toBuilder()..update(updates)).build();
+  User rebuild(void updates(UserBuilder b)) => (toBuilder()..update(updates)).build();
 
   @override
   UserBuilder toBuilder() => new UserBuilder()..replace(this);
@@ -195,7 +193,17 @@ class _$User extends User {
                                                                 $jc(
                                                                     $jc(
                                                                         $jc(
-                                                                            $jc($jc($jc($jc($jc($jc(0, loginUID.hashCode), docUID.hashCode), firstName.hashCode), lastName.hashCode), email.hashCode),
+                                                                            $jc(
+                                                                                $jc(
+                                                                                    $jc(
+                                                                                        $jc(
+                                                                                            $jc(
+                                                                                                $jc(0,
+                                                                                                    loginUID.hashCode),
+                                                                                                docUID.hashCode),
+                                                                                            firstName.hashCode),
+                                                                                        lastName.hashCode),
+                                                                                    email.hashCode),
                                                                                 phoneNumber.hashCode),
                                                                             mobileNumber.hashCode),
                                                                         address.hashCode),
@@ -289,18 +297,15 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   String _dietaryRestrictions;
   String get dietaryRestrictions => _$this._dietaryRestrictions;
-  set dietaryRestrictions(String dietaryRestrictions) =>
-      _$this._dietaryRestrictions = dietaryRestrictions;
+  set dietaryRestrictions(String dietaryRestrictions) => _$this._dietaryRestrictions = dietaryRestrictions;
 
   String _emergencyContactName;
   String get emergencyContactName => _$this._emergencyContactName;
-  set emergencyContactName(String emergencyContactName) =>
-      _$this._emergencyContactName = emergencyContactName;
+  set emergencyContactName(String emergencyContactName) => _$this._emergencyContactName = emergencyContactName;
 
   String _emergencyContactNumber;
   String get emergencyContactNumber => _$this._emergencyContactNumber;
-  set emergencyContactNumber(String emergencyContactNumber) =>
-      _$this._emergencyContactNumber = emergencyContactNumber;
+  set emergencyContactNumber(String emergencyContactNumber) => _$this._emergencyContactNumber = emergencyContactNumber;
 
   String _emergencyContactRelation;
   String get emergencyContactRelation => _$this._emergencyContactRelation;
@@ -309,13 +314,11 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   DateTime _membershipStart;
   DateTime get membershipStart => _$this._membershipStart;
-  set membershipStart(DateTime membershipStart) =>
-      _$this._membershipStart = membershipStart;
+  set membershipStart(DateTime membershipStart) => _$this._membershipStart = membershipStart;
 
   DateTime _membershipRenewal;
   DateTime get membershipRenewal => _$this._membershipRenewal;
-  set membershipRenewal(DateTime membershipRenewal) =>
-      _$this._membershipRenewal = membershipRenewal;
+  set membershipRenewal(DateTime membershipRenewal) => _$this._membershipRenewal = membershipRenewal;
 
   String _disabilities;
   String get disabilities => _$this._disabilities;
@@ -327,8 +330,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   String _medicalIssues;
   String get medicalIssues => _$this._medicalIssues;
-  set medicalIssues(String medicalIssues) =>
-      _$this._medicalIssues = medicalIssues;
+  set medicalIssues(String medicalIssues) => _$this._medicalIssues = medicalIssues;
 
   String _position;
   String get position => _$this._position;
@@ -344,16 +346,14 @@ class UserBuilder implements Builder<User, UserBuilder> {
 
   bool _waiverRelease;
   bool get waiverRelease => _$this._waiverRelease;
-  set waiverRelease(bool waiverRelease) =>
-      _$this._waiverRelease = waiverRelease;
+  set waiverRelease(bool waiverRelease) => _$this._waiverRelease = waiverRelease;
 
   bool _intakeForm;
   bool get intakeForm => _$this._intakeForm;
   set intakeForm(bool intakeForm) => _$this._intakeForm = intakeForm;
 
   ListBuilder<String> _services;
-  ListBuilder<String> get services =>
-      _$this._services ??= new ListBuilder<String>();
+  ListBuilder<String> get services => _$this._services ??= new ListBuilder<String>();
   set services(ListBuilder<String> services) => _$this._services = services;
 
   UserBuilder();
@@ -441,8 +441,7 @@ class UserBuilder implements Builder<User, UserBuilder> {
         _$failedField = 'services';
         services.build();
       } catch (e) {
-        throw new BuiltValueNestedFieldError(
-            'User', _$failedField, e.toString());
+        throw new BuiltValueNestedFieldError('User', _$failedField, e.toString());
       }
       rethrow;
     }
